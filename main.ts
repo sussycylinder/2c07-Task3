8f79e7d99b58c163e9114fc9d926737df6162bc5
sussycylinder <115450379+sussycylinder@users.noreply.github.com>
2022-10-18 21:38:23 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,57 @@
+let count = 0
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(randint(1, 6))
+})
+input.onButtonPressed(Button.B, function () {
+    count = 4
+    basic.showNumber(count)
+    while (count > 0) {
+        count += -1
+        basic.showNumber(count)
+    }
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . # . .
+        . # . # .
+        . . # . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . # . .
+        . # . # .
+        # . . . #
+        . # . # .
+        . . # . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        # . . . #
+        . . . . .
+        # . . . #
+        . # . # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
 basic.forever(function () {
 	
 })
